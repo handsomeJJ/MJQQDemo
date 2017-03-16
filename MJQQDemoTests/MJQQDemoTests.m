43f0c6b7d771dfcaec31d9c92c36002a80431d66
handsomeJJ <727540797@qq.com>
2017-03-16 11:34:09 +0800

    Initial Commit

---
+++ MJQQDemoTests/MJQQDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  MJQQDemoTests.m
+//  MJQQDemoTests
+//
+//  Created by none on 17/3/16.
+//  Copyright © 2017年 MJ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MJQQDemoTests : XCTestCase
+
+@end
+
+@implementation MJQQDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
